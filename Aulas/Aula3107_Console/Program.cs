562c4af82a93c76d9be9f0c8fe1569833b7a6302
nathanael-iung <nathanaeliung@hotmail.com>
2018-07-31 21:25:58 -0300

    Interação com o usuário

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -8,8 +8,20 @@ namespace Aula3107_Console
 {
     class Program
     {
+
+        public static int Id { get; set; }
+
+
         static void Main(string[] args)
-        {
+        {   
+            Console.WriteLine("Hello World...\n");
+            
+            Console.WriteLine("Digite um número: ");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou " + Id);
+
+            Console.ReadKey();
         }
     }
 }
